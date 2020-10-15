22175b0f399b5cab9b6214eed6bc022dbfa4f4a5
Ugo Palatucci <ugo.palatucci@skillbill.it>
2020-10-15 08:58:19 +0200

    init view with header and nav

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+import EPG from './components/EPG';
+import './index.css';
+
+class App extends React.Component{
+  render(){
+    return(
+      <>
+        <Header />
+        <NavBar />
+        <EPG />
+      </>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
